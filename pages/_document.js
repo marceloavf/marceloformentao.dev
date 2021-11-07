89f548b17d79f2de46e624378f411bcda7cc9c9e
Marcelo Formentão <marceloavf3@hotmail.com>
2021-11-07 14:12:17 -0300

    fix: crossorigin type

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100;300;400;600;700;800&display=swap"
             rel="stylesheet"
