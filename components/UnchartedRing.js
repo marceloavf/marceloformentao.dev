438320c883599aae69fb66265a238c0aa68967dc
Marcelo Formentão <marceloavf3@hotmail.com>
2021-11-07 14:14:47 -0300

    fix: add fixed height to uncharted ring

--- components/UnchartedRing.js
+++ components/UnchartedRing.js
@@ -3,9 +3,11 @@ import UnchartedRingControl from './UnchartedRingControl'
 
 const UnchartedRing = () => {
   return (
-    <Canvas concurrent camera={{ fov: 40, near: 0.1, far: 1000, position: [0, 0, 5] }}>
-      <UnchartedRingControl />
-    </Canvas>
+    <div className="h-52">
+      <Canvas concurrent camera={{ fov: 40, near: 0.1, far: 1000, position: [0, 0, 6] }}>
+        <UnchartedRingControl />
+      </Canvas>
+    </div>
   )
 }
 
