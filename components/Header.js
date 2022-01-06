7db1d80510660a1130d49963f6dd26d2c6564dbc
Marcelo Formentão <marceloavf3@hotmail.com>
2022-01-06 08:55:24 -0300

    fix: mobile nav blur

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,31 @@
+import { useState } from 'react'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 
 export default function Header({ isHome }) {
   const { t } = useTranslation()
   const router = useRouter()
+  const [navShow, setNavShow] = useState(false)
   const { locale, locales, defaultLocale } = router
 
   const changeLanguage = (e) => {
     const locale = e.target.value
     router.push(router.asPath, router.asPath, { locale })
+  }
+
+  const onToggleNav = () => {
+    setNavShow((status) => {
+      if (status) {
+        document.body.style.overflow = 'auto'
+      } else {
+        document.body.style.overflow = 'hidden'
+      }
+      return !status
+    })
   }
 
   return (
@@ -56,9 +68,38 @@ export default function Header({ isHome }) {
               <ThemeSwitch />
             </div>
           </div>
-          <MobileNav />
+          <div className="sm:hidden">
+            <button
+              type="button"
+              className="w-8 h-8 ml-1 mr-1 rounded"
+              aria-label="Toggle Menu"
+              onClick={onToggleNav}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="text-gray-900 dark:text-gray-100"
+              >
+                {navShow ? (
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                ) : (
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
         </nav>
       </header>
+      <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
     </>
   )
 }
--- components/MobileNav.js
+++ components/MobileNav.js
@@ -3,68 +3,29 @@ import useTranslation from 'next-translate/useTranslation'
 import Link from './Link'
 import headerNavLinks from '@/data/headerNavLinks'
 
-const MobileNav = () => {
+const MobileNav = ({ navShow, onToggleNav }) => {
   const { t } = useTranslation()
-  const [navShow, setNavShow] = useState(false)
-
-  const onToggleNav = () => {
-    setNavShow((status) => {
-      if (status) {
-        document.body.style.overflow = 'auto'
-      } else {
-        // Prevent scrolling
-        document.body.style.overflow = 'hidden'
-      }
-      return !status
-    })
-  }
 
   return (
     <div className="sm:hidden">
-      <button
-        type="button"
-        className="w-8 h-8 ml-1 mr-1 rounded"
-        aria-label="Toggle Menu"
-        onClick={onToggleNav}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-gray-900 dark:text-gray-100"
-        >
-          {navShow ? (
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          ) : (
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          )}
-        </svg>
-      </button>
       <div
-        className={`fixed w-full h-full top-24 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 ease-in-out duration-300 ${
+        className={`fixed w-full h-screen top-24 right-0 bg-white dark:bg-violet-1000 z-40 ease-in-out duration-500 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
-        }`}
+        } bg-opacity-30 dark:bg-opacity-30 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100`}
       >
         <button
           type="button"
           aria-label="toggle modal"
-          className="fixed w-full h-full cursor-auto focus:outline-none"
+          className="fixed z-30 w-full h-full cursor-auto focus:outline-none"
           onClick={onToggleNav}
         ></button>
-        <nav className="fixed h-full mt-8">
+        <nav className="mt-8 relative space-y-8 z-40">
           {headerNavLinks.map((link) => (
-            <div key={link.title} className="px-12 py-4">
+            <div key={link.title} className="px-12">
               <Link
                 href={link.href}
-                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                title={link.title}
+                className="text-xl font-semibold leading-8 tracking-wide text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white"
                 onClick={onToggleNav}
               >
                 {t(`headerNavLinks:${link.title.toLowerCase()}`)}
