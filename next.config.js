e43a5c952fac4c8c66087bcfd4918cca86aac937
Marcelo Formentão <marceloavf3@hotmail.com>
2022-01-15 14:11:13 -0300

    fix: remove preact, add concurrent, add transpilers
    - Remove preact since it does not support reconciliers

--- next.config.js
+++ next.config.js
@@ -2,45 +2,51 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const withPlugins = require('next-compose-plugins')
+
+const withTM = require('next-transpile-modules')([
+  'three',
+  'react-spring',
+  '@react-three/fiber',
+  '@react-three/postprocessing',
+  '@react-three/drei',
+])
+
 const nextTranslate = require('next-translate')
 
-module.exports = nextTranslate(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['pages', 'components', 'lib', 'layouts', 'scripts', 'context'],
-    },
-    webpack: (config, { dev, isServer }) => {
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|mp4)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts', 'context'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
           },
-        ],
-      })
+        },
+      ],
+    })
 
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
 
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
-        })
-      }
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      exclude: /node_modules/,
+      use: ['file-loader'],
+    })
 
-      return config
-    },
-  })
-)
+    return config
+  },
+}
+
+module.exports = withPlugins([withTM, nextTranslate, withBundleAnalyzer], nextConfig)
