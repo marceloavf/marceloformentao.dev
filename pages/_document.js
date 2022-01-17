d05be600d05365628ec0eb3f6c2cdf58603bfd51
Marcelo Formentão <marceloavf3@hotmail.com>
2022-01-17 10:37:46 -0300

    chore: move scroll-smooth to document

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className="scroll-smooth">
         <Head>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
