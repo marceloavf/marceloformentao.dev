ac70ca4ded0063b313e5c2834f2c41c4e4879bb9
Marcelo Formentão <marceloavf3@hotmail.com>
2022-01-23 21:30:01 -0300

    feat(link): add activelink by page

--- components/Link.js
+++ components/Link.js
@@ -1,15 +1,37 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import React, { Children } from 'react'
 
-const CustomLink = ({ href, ...rest }) => {
+const ActiveLink = ({ children, activeClassName, ...props }) => {
+  const { asPath } = useRouter()
+  const child = Children.only(children)
+  const childClassName = child.props.className || ''
+
+  const className =
+    asPath === props.href || asPath === props.as
+      ? `${childClassName} ${activeClassName}`.trim()
+      : childClassName
+
+  return (
+    <Link {...props}>
+      {React.cloneElement(child, {
+        className: className || null,
+      })}
+    </Link>
+  )
+}
+
+const CustomLink = ({ href, isActive, activeClassName, ...rest }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
+  const BaseLink = isActive ? ActiveLink : Link
 
   if (isInternalLink) {
     return (
-      <Link href={href}>
+      <BaseLink href={href} activeClassName={activeClassName}>
         <a {...rest} />
-      </Link>
+      </BaseLink>
     )
   }
 
