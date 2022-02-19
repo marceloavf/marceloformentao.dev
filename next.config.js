9e53445c8e2e9a972be9907eff116b421c722773
Marcelo Formentão <marceloavf3@hotmail.com>
2022-02-19 15:11:41 -0300

    fix: remove unused loader config

--- next.config.js
+++ next.config.js
@@ -25,19 +25,6 @@ const nextConfig = {
   },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    })
-
-    config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
